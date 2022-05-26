015ff3e4fb1c818e80c265b0fd66f2ea15dee425
lilkraver <88539664+lilkraver@users.noreply.github.com>
2022-05-26 17:13:08 +0700

    Update test_product_page.py
    +going to login page tests

--- test_product_page.py
+++ test_product_page.py
@@ -18,6 +18,18 @@ def test_guest_can_add_product_to_basket(browser, promo_offer):
     page.open()
     page.add_item()
     
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     page = ProductPage(browser, link_for_tree_tests)
     page.open()
