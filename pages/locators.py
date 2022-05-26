faacb03a92982347269f8e4020879e286d9c84d9
lilkraver <88539664+lilkraver@users.noreply.github.com>
2022-05-26 13:50:12 +0700

    Update locators.py
    +locators for ProductPage

--- pages/locators.py
+++ pages/locators.py
@@ -6,3 +6,12 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+class ProductPageLocators():
+    product_url = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207'
+    b_button = (By.CSS_SELECTOR, '#add_to_basket_form [type="submit"]')
+    item_name = (By.CSS_SELECTOR, '.row h1')
+     
+    item_bname = (By.CSS_SELECTOR, '.alertinner>strong')
+    item_price = (By.CSS_SELECTOR, 'p.price_color')
+    total_price = (By.CLASS_NAME, '.alertinner p strong')
