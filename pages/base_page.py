505fe8812a5b31f049011f83ec838a13ecb1e795
lilkraver <88539664+lilkraver@users.noreply.github.com>
2022-05-26 16:33:31 +0700

    Update base_page.py
    +disappearing elements

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,5 +1,8 @@
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import math
 
 class BasePage():
@@ -17,6 +20,22 @@ class BasePage():
     except (NoSuchElementException):
         return False
     return True
+  
+  # ожидание исчезания элемента
+  def is_disappeared(self, how, what, timeout=4):
+    try:
+      WebDriverWait(self.browser, timeout, 1, TimeoutException).until_not(EC.presence_of_element_located((how, what)))
+    except TimeoutException:
+      return False
+    return True
+
+  # поиск элемента, что он не появится в течении заданного времени
+  def is_not_element_present(self, how, what, timeout=4):
+    try:
+      WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
+    except TimeoutException:
+      return True
+    return False
 
   def solve_quiz_and_get_code(self):
         self.browser.implicitly_wait(10)
