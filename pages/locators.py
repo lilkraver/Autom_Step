ccee72da5b88e29e27736f8d65857d22d475be6b
lilkraver <88539664+lilkraver@users.noreply.github.com>
2022-05-26 21:51:36 +0700

    Update locators.py
    +basket page locs

--- pages/locators.py
+++ pages/locators.py
@@ -21,3 +21,7 @@ class ProductPageLocators():
 class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+
+class BasketPageLocators():
+    EMPTY_TEXT = (By.CSS_SELECTOR, "#content_inner > p")
+    ROW_OF_ITEM = (By.CSS_SELECTOR, ".basket-items .row")    
