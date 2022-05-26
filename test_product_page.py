a7195d05af68f943c755f19a5e8c1d388c14af73
lilkraver <88539664+lilkraver@users.noreply.github.com>
2022-05-26 14:16:03 +0700

    Update test_product_page.py
    changin parametrise adding to basket

--- test_product_page.py
+++ test_product_page.py
@@ -4,13 +4,16 @@ from pages.locators import ProductPageLocators
 from pages.base_page import BasePage
 
 
-initial_url = ProductPageLocators.product_url
-urls = [f"{initial_url}/?promo=offer{no}" for no in range(10)]
+#initial_url = ProductPageLocators.product_url
+#urls = [f"{initial_url}/?promo=offer{no}" for no in range(10)]
 
-
-@pytest.mark.parametrize('link', urls)
-def test_guest_can_add_product_to_basket(browser, link):
-    link = ProductPageLocators.product_url
+#@pytest.mark.parametrize('link', urls)
+#def test_guest_can_add_product_to_basket(browser, link):
+    #link = ProductPageLocators.product_url
+    
+@pytest.mark.parametrize('promo_offer', ["0","1", "3", "4", "5", "6", "7", "8", "9"])
+def test_guest_can_add_product_to_basket(browser, promo_offer):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo_offer}"
     page = ProductPage(browser, link)
     page.open()
     page.add_item()
